ae4aa037f8274b2542beb6bb694f829a7b3cd532
Totenfluch <ziegler.christian@kabelmail.de>
2016-10-16 22:19:56 +0200

    Added 20

---
+++ Prog3-1/Prog3-1/Prog3-20.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+void strcpy1(char * s, char * t);
+void strcpy2(char * s, char * t);
+
+int main(void) {
+	char dest1[64];
+	char string1[] = "Hallo meine Studenten!";
+	char dest2[64];
+	char string2[] = "Hallo meine lieben Studenten!";
+	strcpy1(dest1, string1);
+	strcpy2(dest2, string2);
+	printf("%s\n%s\n%s\n%s\n", dest1, string1, dest2, string2);
+	printf("Test1: %d  - Test2: %d | 0 -> Success\n", strcmp(dest1, string1), strcmp(dest2, string2));
+	system("pause");
+	return 0;
+}
+
+void strcpy1(char * s, char * t)
+{
+	int i = 0;
+	while ((s[i] = t[i]) != 0)
+		i++;
+}
+
+void strcpy2(char * s, char * t)
+{
+	while ((*s = *t) != 0)
+	{
+		s++;
+		t++;
+	}
+}
