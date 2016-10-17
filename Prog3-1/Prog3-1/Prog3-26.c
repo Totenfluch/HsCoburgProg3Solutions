5fd2080aeac5cfea4ffd0f415e742cfe2b69e80d
Christian Ziegler <ziegler.christian@kabelmail.de>
2016-10-17 10:37:47 +0200

    Added crappy 26 - will fix

--- Prog3-1/Prog3-1/Prog3-26.c
+++ Prog3-1/Prog3-1/Prog3-26.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include <time.h>
 
 int main(void) {
+	printf("Please enter some Text: \n-> ");
+	char text[512];
+	scanf_s("%512[0-9a-zA-Z ]", &text, 512);
+
+	time_t t;
+	time(&t);
+	
+	printf("[%.24s] %s\n", ctime(&t), &text);
 
 
+	FILE *fp;
+	fp = fopen("memo.txt", "w");
+	if (fp == NULL)
+		exit(-1);
+	fprintf(fp, "[%.24s] %s\n", ctime(&t), &text);
+	fclose(fp);
+
+	system("pause");
+	return 0;
 }
