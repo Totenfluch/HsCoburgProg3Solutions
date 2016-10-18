a750c21aa83063b33906d44a21a5336e348bc4f6
Christian Ziegler <ziegler.christian@kabelmail.de>
2016-10-18 10:53:12 +0200

    finished 26

--- Prog3-1/Prog3-1/Prog3-26.c
+++ Prog3-1/Prog3-1/Prog3-26.c
@@ -2,22 +2,37 @@
 #include <time.h>
 
 int main(void) {
-	printf("Please enter some Text: \n-> ");
+	int rCounter;
+	FILE *rf;
+	rf = fopen("memo.txt", "r");
+	if (rf) {
+		while ((rCounter = getc(rf)) != EOF)
+			putchar(rCounter);
+		fclose(rf);
+	}
+
 	char text[512];
-	scanf_s("%512[0-9a-zA-Z ]", &text, 512);
+	while (1) {
+		printf("Please enter some Text('.' to exit): \n-> ");
+		scanf_s(" %512[0-9a-zA-Z.,-_*'?!§$&/()=°^ ]", &text, 512);
 
-	time_t t;
-	time(&t);
-	
-	printf("[%.24s] %s\n", ctime(&t), &text);
+		printf("\n");
+		if (text[0] == '.')
+			break;
 
+		time_t t;
+		time(&t);
 
-	FILE *fp;
-	fp = fopen("memo.txt", "w");
-	if (fp == NULL)
-		exit(-1);
-	fprintf(fp, "[%.24s] %s\n", ctime(&t), &text);
-	fclose(fp);
+		printf("[%.24s] %s\n", ctime(&t), &text);
+
+		FILE *fp;
+		fp = fopen("memo.txt", "a");
+		if (fp == NULL)
+			exit(-1);
+		fprintf(fp, "[%.24s] %s\n", ctime(&t), &text);
+		fclose(fp);
+		strcpy(text, "");
+	}
 
 	system("pause");
 	return 0;
