aacf2a91193f17e1a0da0c1ec2ccbe26180594a5
Christian Ziegler <ziegler.christian@kabelmail.de>
2016-10-18 12:35:04 +0200

    improved 10

--- Prog3-1/Prog3-1/Prog3-10.c
+++ Prog3-1/Prog3-1/Prog3-10.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 
-#define POTENZ(i) i*i
-#define DRITTEPOTENZ(i) i*i*i
+#define POTENZ(i) ((i)*(i))
+#define DRITTEPOTENZ(i) ((i)*(i)*(i))
 #define PI 3.1415927
 
 int main(void) {
 	float kugelradius = -1;
 	printf("Kugel Radius eingeben: ");
+	fflush(stdin);
 	scanf_s("%f", &kugelradius);
 
 	float kugeloberfläche = 4.0 * PI * POTENZ(kugelradius);
 	float kugelvolumen = 4.0 / 3.0 * PI * DRITTEPOTENZ(kugelradius);
 
 	printf("Kugeloberflaeche: %.2f\nKugelvolumen: %.2f\n", kugeloberfläche, kugelvolumen);
-	scanf("%i", 0);
+	system("pause");
 	return 0;
 }
