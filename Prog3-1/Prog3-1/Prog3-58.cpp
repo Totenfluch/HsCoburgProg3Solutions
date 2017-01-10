6dcbe803bbe9de46620cc5a9030da9257e42479a
Christian <zich1500@stud.hs-coburg.de>
2017-01-10 22:38:43 +0100

    58 completed, 59 contains not working

--- Prog3-1/Prog3-1/Prog3-58.cpp
+++ Prog3-1/Prog3-1/Prog3-58.cpp
@@ -1,18 +1,28 @@
 #include <iostream>
 #include <set>
 #include <time.h>
-#include <algorithm>    // std::random_shuffle
 using namespace std;
 
 int main() {
 	set<int> s1 = set<int>();
+	set<int>::iterator it;
+	srand(time(NULL));
+
 	for (int i = 1; i < 47; i++)
 		s1.insert(i);
-	random_shuffle(s1.begin(), s1.end());
 
-	for (int i = 0; i < 8; i++)
-		cout << "Zahl " << i << " :" << (*s1.begin() + i) << endl;
-	
+	for (int i = 1; i < 8; i++) {
+		it = s1.begin();
+		int rn = (int)(rand() % (46-i));
+		for (int x = 0; x < rn; x++)
+			++it;
+		int my_rand_val = *it;
+		it = s1.erase(it);
+		if (i != 7)
+			cout << "Zahl: " << i << ": " << my_rand_val << endl;
+		else
+			cout << "Superzahl: " << my_rand_val << endl;
+	}
 
 	system("PAUSE");
 	return 0;
---
+++ Prog3-1/Prog3-1/Prog3-59.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <string>
+#include <list>
+#include <algorithm>
+using namespace std;
+
+
+class Person {
+private:
+
+	string name;
+public:
+	Person(const string& nm) : name(nm) {};
+	const string& getName() const { return name; };
+};
+
+std::ostream& operator<<(std::ostream& os, const Person& obj)
+{
+	os << obj.getName();
+	return os;
+}
+
+bool operator== (const Person &c1, const Person &c2)
+{
+	return c1.getName().compare(c2.getName());
+}
+
+class Kontakte {
+private:
+	list<Person> kontakt;
+public:
+	Kontakte() {};
+	void printAll();
+	Person contains(string n);
+	void addContact(Person p);
+};
+
+void Kontakte::printAll() {
+	for (list<Person>::iterator i = kontakt.begin(); i != kontakt.end(); ++i)
+		cout << *i << endl;
+}
+
+
+void Kontakte::addContact(Person p) {
+	kontakt.push_front(p);
+}
+
+Person Kontakte::contains(string n) {
+	auto result = find(kontakt.begin(), kontakt.end(), Person(n));
+	if (result != kontakt.end())
+		return *result;
+	else
+		return "Not found";
+}
+
+int main() {
+	Person p1 = Person("Peter");
+	Person p2 = Person("Dieter");
+	Person p3 = Person("Brake");
+	Person p4 = Person("Bingo");
+	Person p5 = Person("Stefan");
+
+	Kontakte conts = Kontakte();
+	conts.addContact(p1);
+	conts.addContact(p2);
+	conts.addContact(p3);
+	conts.addContact(p4);
+	conts.addContact(p5);
+
+	cout << conts.contains("Peter") << " " << conts.contains("Lol") << endl;
+	conts.printAll();
+	system("PAUSE");
+	return 0;
+}
