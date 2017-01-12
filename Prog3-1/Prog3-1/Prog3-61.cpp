87988ef48054767964b1460f979c335738cfa480
Christian Ziegler <ziegler.christian@kabelmail.de>
2017-01-12 15:22:35 +0100

    61 complete needs bugfix and test case

--- Prog3-1/Prog3-1/Prog3-61.cpp
+++ Prog3-1/Prog3-1/Prog3-61.cpp
@@ -24,8 +24,8 @@ public:
 	friend    bool operator!=   (const Set&, const Set&);  // Ungleichheit
 	friend    Set  operator*    (const Set&, const Set&);  // Schnitt
 	friend    Set  operator+    (const Set&, const Set&);  // Vereinigung
-	friend    Set  operator-    (const Set&, const Set&);  // Mengenminus
-	friend    bool operator<=   (const Set&, const Set&);  // Teilmenge
+	friend    Set  operator-    (const Set&, const Set&);  // Mengenminus a-b
+	friend    bool operator<=   (const Set&, const Set&);  // Teilmenge 
 	friend    ostream& operator<< (ostream&, const Set&); // Ausgabe
 	friend    istream& operator >> (istream&, Set&); // Eingabe eines Elements
 
@@ -34,7 +34,40 @@ public:
 
 };
 
-Set::Set(int size = 50) : maxCard(size) {
+Set::ErrCode Set::AddElem(int i) {
+	if (card != maxCard) {
+		elems[i] = i;
+		card++;
+	}
+	else
+		return ADDERR;
+	return OK;
+}
+
+Set::ErrCode Set::RmvElem(int i){
+	int found = -1;
+	for (int i = 0; i < card; i++) {
+		if(elems[i] == i){
+			found = i;
+			break;
+		}
+	}
+	if (found == -1)
+		return RMVERR;
+	int* newElems = new int[maxCard];
+	for (int i = 0; i < found; i++)
+		newElems[i] = elems[i];
+
+	for (int i = found + 1; i < card; i++)
+		newElems[i] = elems[i];
+	
+	delete[] elems;
+	elems = newElems;
+
+	return OK;
+}
+
+Set::Set(int size) : maxCard(size) {
 	elems = new int[size];
 }
 
@@ -55,6 +88,7 @@ Set& Set::operator=(const Set& anotherSet){
 	card = anotherSet.card;
 	delete[] elems;
 	elems = anotherSet.elems;
+	return *this;
 }
 
 bool operator&(int i, Set s) {
@@ -105,10 +139,90 @@ Set  operator*    (const Set& s1, const Set& s2) {
 	Set s3 = Set(schnittElems);
 	s3.maxCard = schnittElems;
 	s3.card = schnittElems;
-	s3.elems = new int[schnittElems];
 
 	for (int i = 0; i < schnittElems; i++)
 		s3.elems[i] = partElems[i];
 
 	return s3;
+}
+
+Set  operator+    (const Set& s1, const Set& s2) {
+	Set s3 = Set(s1.maxCard + s2.maxCard);
+	s3.card = s1.card + s2.card;
+	for (int i = 0; i < s1.maxCard; i++)
+		s3.elems[i] = s1.elems[i];
+	for (int i = 0; i < s2.maxCard; i++)
+		s3.elems[i + s1.maxCard - 1] = s2.elems[i];
+	return s3;
+}
+
+Set  operator-    (const Set& s1, const Set& s2) {
+	int* partElems = new int[s1.maxCard];
+	int schnittElems = 0;
+	for (int i = 0; i < s1.maxCard; i++) {
+		for (int x = 0; x < s2.maxCard; i++) {
+			bool found = false;
+			if (s1.elems[i] == s2.elems[i]) {
+				found = true;
+				break;
+				if (x >= s1.maxCard)
+					break;
+			}
+			if(!found)
+				partElems[schnittElems++] = s1.elems[i];
+		}
+	}
+
+	Set s3 = Set(schnittElems);
+	s3.maxCard = schnittElems;
+	s3.card = schnittElems;
+
+	for (int i = 0; i < schnittElems; i++)
+		s3.elems[i] = partElems[i];
+
+	return s3;
+}
+
+bool operator<=   (const Set& s1, const Set& s2) {
+	if (s1.maxCard > s2.maxCard)
+		return false;
+	for (int i = 0; i < s1.maxCard; i++) {
+		bool found = false;
+		for (int x = 0; x < s2.maxCard; x++) {
+			if (s1.elems[i] == s2.elems[x]);
+				found = true;
+		}
+		if (!found)
+			return false;
+	}
+	return true;
+}
+
+ostream& operator<< (ostream& os, const Set& s1){
+	cout << "Set (" << s1.card << "/" << s1.maxCard << ")" << endl << "Elemenete: ";
+	for (int i = 0; i < s1.maxCard; i++)
+		if (i != s1.maxCard - 1)
+			cout << s1.elems[i] + ", ";
+		else
+			cout << s1.elems[i];
+	cout << endl;
+	return os;
+}
+
+istream& operator >> (istream& is, Set& s1) {
+	cout << "Anzahl der Elemente Eingeben: " << endl;
+	int i = 0;
+	cin >> i;
+	cout << "Bitte " << i << " Elemente eingeben." << endl;
+	for (int i = 0; i < s1.maxCard; i++)
+		is >> s1.elems[i];
+	return is;
+}
+
+int main() {
+	Set s1 = Set();
+	cin >> s1;
+	cout << s1;
+	system("PAUSE");
+	return 0;
 }
